6b15fd024c86bdb85b0918993eaf8164c09dac0e
ACalza <albert.calzaretto@gmail.com>
2015-06-13 13:44:32 -0400

    Async issues for populate DB, works now if you add them individually

--- bin/populateDB.js
+++ bin/populateDB.js
@@ -9,8 +9,9 @@ var Class = require('../models/class');
 var Phylum = require('../models/phylum');
 var Domain = require('../models/domain');
 var addSpecie = require('../lib/species');
-var fs = require('fs')
+var fs = require('fs');
 
+var async = require('async');
 //inspired by http://techslides.com/convert-csv-to-json-in-javascript
 fs.readFile('../database/modelspecies.csv', 'utf8', function(err, data){
 	if(err){
@@ -20,12 +21,55 @@ fs.readFile('../database/modelspecies.csv', 'utf8', function(err, data){
 	
 	var lines = data.split("\r");
     var headers = lines[0].split(",");
-	
 	var domain, phylum, clas, order, family, genus, species;
-    
-	for(var i=4; i<lines.length; i++) {
+  	lines.splice(0, 0);
+	//async really pisses me off...
+	var iterator = function(index){
+		if(index === lines.length)
+			return;
+		var column = {};
+  	  	var currentline = lines[index].split(",");
+ 	   	//put species
+  	  	for(var j=headers.length - 5;j<headers.length;j++){
+  			  column[headers[j]] = currentline[j];
+  	  	}
+	
+		column["domain"] = currentline[0];
+		column["phylum"] = currentline[1];
+		column["class"] = currentline[2];
+		column["order"] = currentline[3];
+		column["family"] = currentline[4];
+		column["genus"] = currentline[5];
+		
+		addSpecie(column, function(row){
+			console.log(index);
+			//iterator(index + 1);
+		});
+	}
+	iterator(5);
+	//remove first index
+	//for(var i=4; i<lines.length; i++) {
 		//meant to say row, but was too lazy to rename
-  	  	var column = {};
+  	/*async.eachSeries(lines, function iterator(line, callback){
+		var column = {};
+  	  	var currentline = line.split(",");
+ 	   	//put species
+  	  	for(var j=headers.length - 5;j<headers.length;j++){
+  			  column[headers[j]] = currentline[j];
+  	  	}
+		column["domain"] = currentline[0];
+		column["phylum"] = currentline[1];
+		column["class"] = currentline[2];
+		column["order"] = currentline[3];
+		column["family"] = currentline[4];
+		column["genus"] = currentline[5];
+		console.log(column);
+		addSpecie(column, function(row){
+			callback();
+		});
+  	});
+		
+		var column = {};
 
   	  	var currentline = lines[i].split(",");
  	   	//put species
@@ -39,12 +83,14 @@ fs.readFile('../database/modelspecies.csv', 'utf8', function(err, data){
 		column["family"] = currentline[4];
 		column["genus"] = currentline[5];
 		
-		addSpecie(column);
+		addSpecie(column, function(row){
+			
+		});
 		return;
 		
 		
 		
-    }	
+    }*/
 });
 
 
