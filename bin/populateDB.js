22a3b3659f35023c78e7b60cadfdb986530d8783
ACalza <albert.calzaretto@gmail.com>
2015-06-13 11:29:08 -0400

    basic structure of recursion seems to be working

--- bin/populateDB.js
+++ bin/populateDB.js
@@ -24,22 +24,24 @@ fs.readFile('../database/modelspecies.csv', 'utf8', function(err, data){
 	var domain, phylum, clas, order, family, genus, species;
     
 	for(var i=4; i<lines.length; i++) {
-  	  	var s = {}, g = {}, f ={}, o= {}, c = {}, p = {}, d = {};
+		//meant to say row, but was too lazy to rename
+  	  	var column = {};
+
   	  	var currentline = lines[i].split(",");
  	   	//put species
   	  	for(var j=headers.length - 5;j<headers.length;j++){
-  			  s[headers[j]] = currentline[j];
+  			  column[headers[j]] = currentline[j];
   	  	}
-		s.ename = currentline[6];
-		g.ename = currentline[5];
-		f.ename = currentline[4];
-		o.ename = currentline[3];
-		c.ename = currentline[2];
-		p.ename = currentline[1];
-		d.ename = currentline[0];
+		column["domain"] = currentline[0];
+		column["phylum"] = currentline[1];
+		column["class"] = currentline[2];
+		column["order"] = currentline[3];
+		column["family"] = currentline[4];
+		column["genus"] = currentline[5];
 		
-		addSpecie(s, g, f, o, c, p, d);
-		return; 
+		addSpecie(column);
+		return;
+		
 		
 		
     }	
